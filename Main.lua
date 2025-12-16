ddc24fd9b2a865362aa14448b95c0fbe53bdca55
Bill Van Ark <4hchm4xub2@snkmail.com>
2025-12-16 20:35:31 +0100

    Renamed TimerControl file to be more obviously Hobnanigans-related.

--- Main.lua
+++ Main.lua
@@ -25,7 +25,7 @@ import "CubePlugins.FestivalBuddyII.Globals";
 --import "CubePlugins.FestivalBuddyII.Libraries.Tooltip";
 import "CubePlugins.FestivalBuddyII.ChatLogger";
 import "CubePlugins.FestivalBuddyII.BarterItems";
-import "CubePlugins.FestivalBuddyII.TimerControl";
+import "CubePlugins.FestivalBuddyII.HobnanigansTimerControl";
 import "CubePlugins.FestivalBuddyII.QuickGuides.QuickGuides";
 import "CubePlugins.FestivalBuddyII.QuickGuides.InnLeagueAleAssociation.QuickGuideStrings_InnLeagueAleAssociation";
 import "CubePlugins.FestivalBuddyII.QuickGuides.Midsummer.QuickGuideStrings_Midsummer";
