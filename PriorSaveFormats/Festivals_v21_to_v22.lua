00a8444b1313f85eed080ad440fb1746266d187d
Bill Van Ark <4hchm4xub2@snkmail.com>
2026-06-30 22:33:23 +0200

    Added comments

--- PriorSaveFormats/Festivals_v21_to_v22.lua
+++ PriorSaveFormats/Festivals_v21_to_v22.lua
@@ -18,16 +18,21 @@ function Update_FestivalBuddySettings_from_v21_to_v22(settings)
             settings.QUICK_GUIDE_QUESTS_TO_USE[festival] = {};
 
             for guideId, guideName in ipairs(_G.CubePlugins.FestivalBuddyII._QUICK_GUIDES[festival]) do
+                -- Now that the old settings are saved off, start from scratch,
                 settings.QUICK_GUIDE_QUESTS_TO_USE[festival][guideId] = {};
+                -- Copy any defaulted settings in:
                 if (oldSettings[guideId]) then
                     settings.QUICK_GUIDE_QUESTS_TO_USE[festival][guideId] = deepcopy(oldSettings[guideId]);
                 end
             end
 
+            -- Then overwrite with any previously-existing values:
             for questName, value in pairs(oldSettings) do
                 if (type(value) == "boolean") then
                     local index = 1;
                     settings.QUICK_GUIDE_QUESTS_TO_USE[festival][index][questName] = value;
+
+                    -- Duplicate the wrapper to each of Minas Tirith and Furtherholm:
                     if (questName == "IN_CELEBRATION_OF_MIDSUMMER") then
                         settings.QUICK_GUIDE_QUESTS_TO_USE[festival][1][questName] = value;
                         settings.QUICK_GUIDE_QUESTS_TO_USE[festival][2][questName] = value;
